c30556f924ea351be1d227d71b3c7599333862d4
Antoine GALPY <antoine@galpy.net>
2021-03-02 11:25:02 +0100

    Ajout des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :boats , only: :index
+  resources :boats , only: :show do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :users, only: [:new, :create]
+  namespace :owner do
+    resources :boats , only: [:new, :create]
+    resources :bookings, only: [:index, :update]
+  end
+  namespace :renter do
+    resources :bookings, only: [:index, :update]
+  end
 end
