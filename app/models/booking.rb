e16f5fe28707e3b84284482ffb9176fc6a8295b9
Guillaume Ferchaud <guillaumeferchaud@hotmail.fr>
2021-03-03 11:19:34 +0100

    add validation on boat for booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :renter, class_name: "User"
   belongs_to :boat
 
-  validates :start_date, :end_date, presence: true
+  validates :start_date, :end_date, :boat, presence: true
 
   STATUSES = ["En attente", "Validée", "Refusée"]
   validates :booking_status, inclusion: { in: STATUSES }
