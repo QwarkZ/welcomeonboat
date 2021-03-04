10a13dfffd1f529e83b97e88aeb9f3441ee269a9
lucrece gugelot <lucrecegugelot@gmail.com>
2021-03-04 16:16:11 +0100

    add-photo-new-boat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  def status(booking)
+    if booking == "En attente"
+      "blue"
+
+
+    elsif booking == "Validée"
+      "valided"
+    else
+      "refused"
+    end
+  end
+
+
+
+def icon(booking)
+    if booking == "En attente"
+      "⏰"
+
+    elsif booking == "Validée"
+      "✅"
+    else
+      "❌"
+    end
+  end
 end
+
