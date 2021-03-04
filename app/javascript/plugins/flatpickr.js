9cbf936540668869cd867448c064c611f2aabcc3
Guillaume Ferchaud <guillaumeferchaud@hotmail.fr>
2021-03-04 17:34:43 +0100

    flatpickr

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,10 @@
 import flatpickr from "flatpickr";
+import rangePlugin from 'flatpickr/dist/plugins/rangePlugin';
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {});
+  flatpickr(".datepicker", {
+    "plugins": [new rangePlugin({ input: "#secondRangeInput" })]
+  });
 }
 
 export { initFlatpickr };
