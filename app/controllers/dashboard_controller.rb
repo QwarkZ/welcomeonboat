0b17a6e500266049193f527e5073d8057bc55e54
Antoine GALPY <antoine@galpy.net>
2021-03-05 12:32:05 +0100

    Correction des orders dans myBookings et bookings, francisation du calendrier de résa

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,18 @@
 class DashboardController < ApplicationController
   def index
     @my_boats = Boat.where("owner_id = ?", current_user.id)
-    @my_pending_bookings = Booking.joins(:boat).where("renter_id = ?", current_user.id).where("start_date > ?", Time.now).where("booking_status = ?", "En attente")
+    @my_pending_bookings = Booking.joins(:boat).where("renter_id = ?", current_user.id).where("start_date >= ?", Time.now).where("booking_status = ?", "En attente").order("start_date")
     @my_future_bookings = Booking.joins(:boat).where("renter_id = ?", current_user.id).where("start_date > ?", Time.now).where("booking_status = ?", "Validée")
     @my_other_bookings = Booking.joins(:boat).where("renter_id = ?", current_user.id).where("start_date < ?", Time.now)
-    @pending_people_rentings = Booking.joins(:boat).where("owner_id = ?", current_user.id).where("start_date > ?", Time.now).where("booking_status = ?", "En attente")
-    @future_people_rentings = Booking.joins(:boat).where("owner_id = ?", current_user.id).where("start_date > ?", Time.now).where("booking_status = ?", "Validée")
-    @other_people_rentings = Booking.joins(:boat).where("owner_id = ?", current_user.id).where("start_date < ?", Time.now)
+
+
+    @pending_people_rentings = Booking.joins(:boat).where("owner_id = ?", current_user.id).where("start_date > ?", Time.now).where("booking_status = ?", "En attente").order(id: :desc)
+
+
+
+
+    @future_people_rentings = Booking.joins(:boat).where("owner_id = ?", current_user.id).where("start_date > ?", Time.now).where("booking_status = ?", "Validée").order(id: :desc)
+    @other_people_rentings = Booking.joins(:boat).where("owner_id = ?", current_user.id).where("start_date < ?", Time.now).order(id: :desc)
   end
 
 end
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,11 +1,13 @@
 import flatpickr from "flatpickr";
 import rangePlugin from 'flatpickr/dist/plugins/rangePlugin';
+import { French } from "flatpickr/dist/l10n/fr.js";
+
 
 const initFlatpickr = () => {
   flatpickr(".datepicker", {
-    "plugins": [new rangePlugin({ input: "#secondRangeInput" })]
+    "plugins": [new rangePlugin({ input: "#secondRangeInput" })],
+    "locale": French
   });
 }
 
 export { initFlatpickr };
-
