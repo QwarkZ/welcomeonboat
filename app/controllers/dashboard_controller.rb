33c5fff5d13f9a2fddb99b2ec5220033fad3af24
marion petitprez <marion.petitprez@gmail.com>
2021-04-21 15:20:23 +0200

    modif front

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,8 @@ class DashboardController < ApplicationController
   def index
     @my_boats = Boat.where("owner_id = ?", current_user.id)
     @my_pending_bookings = Booking.joins(:boat).where("renter_id = ?", current_user.id).where("start_date >= ?", Time.now).where("booking_status = ?", "En attente").order("start_date")
-    @my_future_bookings = Booking.joins(:boat).where("renter_id = ?", current_user.id).where("start_date > ?", Time.now).where("booking_status = ?", "Validée")
-    @my_other_bookings = Booking.joins(:boat).where("renter_id = ?", current_user.id).where("start_date < ?", Time.now)
+    @my_future_bookings = Booking.joins(:boat).where("renter_id = ?", current_user.id).where("start_date > ?", Time.now).where("booking_status = ?", "Validée").order("start_date")
+    @my_other_bookings = Booking.joins(:boat).where("renter_id = ?", current_user.id).where("start_date < ?", Time.now).order("start_date")
 
 
     @pending_people_rentings = Booking.joins(:boat).where("owner_id = ?", current_user.id).where("start_date > ?", Time.now).where("booking_status = ?", "En attente").order(id: :desc)
